78066bce10fd5af258c03584d42705af20e5d28c
Edwin Montoya Munera <emontoya@eafit.edu.co>
2019-03-13 07:58:54 -0500

    update deploy.sh script

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!!3 \n");
+  response.end("Hello NodeApp!!4 \n");
 });
 
 // listen on localhost:8000
