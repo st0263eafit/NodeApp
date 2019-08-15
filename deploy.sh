7c40c28a013836f516022ec966532903097d5191
Edwin Montoya Munera <emontoya@eafit.edu.co>
2019-08-15 10:31:55 -0500

    update to 13

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker kill mynodeapp > /dev/null 2>&1
-docker rm mynodeapp > /dev/null 2>&1
-docker run -d --name mynodeapp -p 80:8000 emontoya/nodeapp
+#docker kill mynodeapp > /dev/null 2>&1
+#docker rm mynodeapp > /dev/null 2>&1
+#docker run -d --name mynodeapp -p 80:8000 emontoya/nodeapp
+/usr/local/bin/docker-compose up
